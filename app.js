192c8b39964333dfb9f12401ace5ffaafa4a9a2d
Alphonso <alphonsotran@gmail.com>
2020-04-17 12:01:19 +0200

    Add body-parser packagee

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 require('./db');
+
 const app = express();
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World');
