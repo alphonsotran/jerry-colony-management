3d847b8973e3aa3d792d633ea24c9beb15dd98c6
Alphonso Tran <alphonsotran@gmail.com>
2020-04-19 16:30:56 +0200

    Add root query to fetch all cages

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphQlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
+const Cage = require('./db/models').Cage;
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+app.use(
+  '/graphql',
+  graphQlHttp({
+    schema: buildSchema(`
+      type Cage {
+        id: ID!
+        genotype: String!
+        cageNumber: String!
+      }
+
+      type RootQuery {
+        cages: [Cage!]!
+      }
+
+    rootValue: {
+      cages: () => {
+        return Cage.findAll()
+          .then((resp) => resp)
+          .catch((err) => {
+            console.log('Unable to fetch cages: ', err);
+            throw err;
+          });
+      },
+    },
+  }),
+);
+
 
 app.listen(3001, () => {
   console.log('listening on port 3001');
