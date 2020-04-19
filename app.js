7d13b3af5238e9febd91fe11fa3cb355189f9677
Alphonso Tran <alphonsotran@gmail.com>
2020-04-19 16:35:43 +0200

    Add mutation to create user. Use bcrypt to hash passwords

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const graphQlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
+const bcrypt = require('bcrypt');
 const Cage = require('./db/models').Cage;
+const User = require('./db/models').User;
 
 const app = express();
 
@@ -18,12 +20,25 @@ app.use(
         cageNumber: String!
       }
 
+      type User {
+        id: ID!
+        firstName: String!
+        lastname: String!
+        email: String!
+        passwordHash: String
+      }
 
       input CageInput {
         genotype: String!
         cageNumber: String!
       }
 
+      input UserInput {
+        firstName: String!
+        lastName: String!
+        email: String!
+        passwordHash: String!
+      }
 
       type RootQuery {
         cages: [Cage!]!
@@ -31,6 +46,7 @@ app.use(
 
       type RootMutation {
         createCage(cageInput: CageInput): Cage
+        createUser(userInput: UserInput): User
       }
 
       schema {
@@ -55,6 +71,24 @@ app.use(
           .then((resp) => resp)
           .catch((err) => {
             console.log('Unable to create cages: ', err);
+            throw err;
+          });
+      },
+      createUser: (args) => {
+        return bcrypt
+          .hash(args.userInput.passwordHash, 12)
+          .then((hashedPassword) =>
+            User.create({
+              firstName: args.userInput.firstName,
+              lastName: args.userInput.lastName,
+              email: args.userInput.email,
+              passwordHash: hashedPassword,
+            }).then((user) => {
+              return { ...user.dataValues, passwordHash: null };
+            }),
+          )
+          .catch((err) => {
+            console.log('Unable to create user: ', err);
             throw err;
           });
       },
