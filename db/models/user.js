43c78f75e9e9e7ef1995244a80d00bae318a777e
Alphonso <alphonsotran@gmail.com>
2020-04-19 15:25:44 +0200

    Create User model

---
+++ db/models/user.js
@@ -0,0 +1,32 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    'User',
+    {
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          isEmail: true,
+        },
+      },
+      passwordHash: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {},
+  );
+  User.associate = function (models) {
+    User.hasMany(models.Cage);
+  };
+  return User;
+};
