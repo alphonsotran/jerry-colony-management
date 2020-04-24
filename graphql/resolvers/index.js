2a397a4a76e2aaa2e4f171c3dec851b813fd8c8a
Alphonso Tran <alphonsotran@gmail.com>
2020-04-24 12:10:14 +0200

    Change resolvers to use async await

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -12,54 +12,51 @@ const user = async (userID) => {
 };
 
 module.exports = {
-  cages: () => {
-    return Cage.findAll()
-      .then((cages) => {
-        return cages.map((cage) => {
-          return {
-            ...cage.dataValues,
-            supervisor: user(cage.dataValues.userId),
-          };
-        });
-      })
-      .catch((err) => {
-        console.log('Unable to fetch cages: ', err);
-        throw err;
+  cages: async () => {
+    try {
+      const cages = await Cage.findAll();
+      return cages.map((cage) => {
+        return {
+          ...cage.dataValues,
+          supervisor: user(cage.dataValues.userId),
+        };
       });
+    } catch (err) {
+      console.log('Unable to fetch cages: ', err);
+      throw err;
+    }
   },
-  createCage: (args) => {
-    return Cage.create({
-      genotype: args.cageInput.genotype,
-      cageNumber: args.cageInput.cageNumber,
-      userId: 1,
-    })
-      .then((cage) => cage)
-      .catch((err) => {
-        console.log('Unable to create cages: ', err);
-        throw err;
+  createCage: async (args) => {
+    try {
+      return await Cage.create({
+        genotype: args.cageInput.genotype,
+        cageNumber: args.cageInput.cageNumber,
+        userId: 1,
       });
+    } catch (err) {
+      console.log('Unable to create cages: ', err);
+      throw err;
+    }
   },
-  createUser: (args) => {
-    return User.findOne({ where: { email: args.userInput.email } })
-      .then((user) => {
-        if (user) {
-          throw new Error('User already exists.');
-        }
-        return bcrypt.hash(args.userInput.passwordHash, 12);
-      })
-      .then((hashedPassword) =>
-        User.create({
-          firstName: args.userInput.firstName,
-          lastName: args.userInput.lastName,
-          email: args.userInput.email,
-          passwordHash: hashedPassword,
-        }).then((user) => {
-          return { ...user.dataValues, passwordHash: null };
-        }),
-      )
-      .catch((err) => {
-        console.log('Unable to create user: ', err);
-        throw err;
+  createUser: async (args) => {
+    try {
+      const user = await User.findOne({
+        where: { email: args.userInput.email },
       });
+      if (user) {
+        throw new Error('User already exists.');
+      }
+      const hashedPassword = await bcrypt.hash(args.userInput.passwordHash, 12);
+      const newUser = await User.create({
+        firstName: args.userInput.firstName,
+        lastName: args.userInput.lastName,
+        email: args.userInput.email,
+        passwordHash: hashedPassword,
+      });
+      return { ...newUser.dataValues, passwordHash: null };
+    } catch (err) {
+      console.log('Unable to create user: ', err);
+      throw err;
+    }
   },
 };
