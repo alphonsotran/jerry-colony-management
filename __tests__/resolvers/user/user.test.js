b9dfb35b31d0e6e7b81afbdf0f4d58d3aa58a1a2
Alphonso Tran <alphonsotran@gmail.com>
2020-04-29 18:57:34 +0200

    Add test for user resolver

---
+++ __tests__/resolvers/user/user.test.js
@@ -0,0 +1,22 @@
+const faker = require('faker');
+const resolvers = require('../../../graphql/resolvers/user');
+
+describe('User Resolvers', () => {
+  test('creates a new user', async () => {
+    const args = {
+      userInput: {
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        email: faker.internet.email(),
+        passwordHash: 'password',
+      },
+    };
+
+    const newUser = await resolvers.createUser(args);
+
+    expect.assertions(3);
+    expect(newUser.firstName).toEqual(args.userInput.firstName);
+    expect(newUser.lastName).toEqual(args.userInput.lastName);
+    expect(newUser.email).toEqual(args.userInput.email);
+  });
+});
